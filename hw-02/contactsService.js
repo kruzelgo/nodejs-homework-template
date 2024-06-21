da60860821bfb11a18d53d0d6311867f1de801ea
kruzelgo <kruzelgo@gmail.com>
2024-06-21 21:29:25 +0200

    update

--- hw-02/contactsService.js
+++ hw-02/contactsService.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const contactsPath = path.join(__dirname, "../contacts.json");
+const contactsPath = path.join(__dirname, "./data/contacts.json");
 
 async function listContacts() {
   const data = await fs.readFile(contactsPath, "utf8");

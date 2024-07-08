3be0cd159b308ceb3be9c6444657169f91801142
kruzelgo <kruzelgo@gmail.com>
2024-07-08 20:07:11 +0200

    Create folder middleware & Add validation middleware for signup and login

---
+++ middlewares/validation.js
@@ -0,0 +1,34 @@
+const Joi = require("joi");
+
+const validateSignup = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  next();
+};
+
+const validateLogin = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateSignup,
+  validateLogin,
+};

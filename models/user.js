1b792aef3c61949134f7a490409fda32e8115a4e
kruzelgo <kruzelgo@gmail.com>
2024-07-08 19:10:32 +0200

    Add password hashing and validation methods to user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
   password: {
@@ -22,5 +23,14 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(6));
+};
+
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
 const User = model("user", userSchema);
+
 module.exports = User;

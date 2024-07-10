f9e7c96b6569aa8d1fe57cf92fe2e54424b9a6f2
kruzelgo <kruzelgo@gmail.com>
2024-07-10 13:12:25 +0200

    Implement user authentication middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const authenticate = async (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decoded.id, token });
+
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+};
+
+module.exports = authenticate;

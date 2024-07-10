c1aaf757302d629291f36b024759dc2796af8f03
kruzelgo <kruzelgo@gmail.com>
2024-07-10 14:47:24 +0200

    Updated user.js and added userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,81 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+exports.signup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "Email already in use" });
+    }
+
+    const newUser = new User({ email });
+    newUser.setPassword(password);
+    await newUser.save();
+
+    res.status(201).json({
+      user: {
+        email: newUser.email,
+        subscription: newUser.subscription,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !user.validPassword(password)) {
+      return res.status(401).json({ message: "Email or password is wrong" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "5d",
+    });
+
+    user.token = token;
+    await user.save();
+
+    res.status(200).json({
+      token,
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.logout = async (req, res) => {
+  try {
+    const user = req.user;
+    user.token = null;
+    await user.save();
+    res.status(204).json();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.getCurrentUser = async (req, res) => {
+  try {
+    const user = req.user;
+    res.status(200).json({
+      email: user.email,
+      subscription: user.subscription,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,91 +1,15 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const User = require("../../models/user");
+const router = express.Router();
+const userController = require("../../controllers/userController");
 const {
   validateSignup,
   validateLogin,
 } = require("../../middlewares/validation");
 const auth = require("../../middlewares/auth");
 
-const router = express.Router();
-
-router.post("/signup", validateSignup, async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(409).json({ message: "Email already in use" });
-    }
-
-    const newUser = new User({ email });
-    newUser.setPassword(password);
-    await newUser.save();
-
-    res.status(201).json({
-      user: {
-        email: newUser.email,
-        subscription: newUser.subscription,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-router.post("/login", validateLogin, async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user || !user.validPassword(password)) {
-      return res.status(401).json({ message: "Email or password is wrong" });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "5d",
-    });
-
-    user.token = token;
-    await user.save();
-
-    res.status(200).json({
-      token,
-      user: {
-        email: user.email,
-        subscription: user.subscription,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-router.get("/logout", auth, async (req, res) => {
-  try {
-    const user = req.user;
-    user.token = null;
-    await user.save();
-    res.status(204).json();
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-router.get("/current", auth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.status(200).json({
-      email: user.email,
-      subscription: user.subscription,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+router.post("/signup", validateSignup, userController.signup);
+router.post("/login", validateLogin, userController.login);
+router.get("/logout", auth, userController.logout);
+router.get("/current", auth, userController.getCurrentUser);
 
 module.exports = router;

28c16d02bd9f54958aa81e06c4292587bce9ddc5
kruzelgo <kruzelgo@gmail.com>
2024-07-10 13:19:01 +0200

    Refactor validation functions to use consistent variable names

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,34 +1,27 @@
 const Joi = require("joi");
 
-const validateSignup = (req, res, next) => {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-  });
+const schemaSignup = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
 
-  const { error } = schema.validate(req.body);
+const schemaLogin = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+exports.validateSignup = (req, res, next) => {
+  const { error } = schemaSignup.validate(req.body);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
-
   next();
 };
 
-const validateLogin = (req, res, next) => {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  });
-
-  const { error } = schema.validate(req.body);
+exports.validateLogin = (req, res, next) => {
+  const { error } = schemaLogin.validate(req.body);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
-
   next();
-};
-
-module.exports = {
-  validateSignup,
-  validateLogin,
 };

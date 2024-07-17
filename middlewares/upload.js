d1361cd7b4152c77b386fa62e3f07c80ee0e720e
kruzelgo <kruzelgo@gmail.com>
2024-07-17 18:31:49 +0200

    Added Multer configuration for file uploads

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -18,11 +18,20 @@ const storage = multer.diskStorage({
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only JPEG and PNG images are allowed!"), false);
+  }
+};
+
 const upload = multer({
-  storage,
+  storage: storage,
   limits: {
     fileSize: 1024 * 1024 * 5, // 5MB limit
   },
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;

1a5d675ecb6cc9677c5d42fe7441de7764841ffa
kruzelgo <kruzelgo@gmail.com>
2024-07-17 11:54:58 +0200

    add avatars url endpoint
    add folder public to gitignore

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const contactController = require("../../controllers/contactController");
+const userController = require("../../controllers/userController");
+const {
+  validateSignup,
+  validateLogin,
+} = require("../../middlewares/validation");
 const auth = require("../../middlewares/auth");
+const upload = require("../../middlewares/upload");
 
-router.get("/", auth, contactController.getAllContacts);
-router.get("/:id", auth, contactController.getContactById);
-router.post("/", auth, contactController.addContact);
-router.delete("/:id", auth, contactController.deleteContact);
-router.put("/:id", auth, contactController.updateContact);
-router.patch("/:id/favorite", auth, contactController.toggleFavorite);
+router.post("/signup", validateSignup, userController.signup);
+router.post("/login", validateLogin, userController.login);
+router.get("/logout", auth, userController.logout);
+router.get("/current", auth, userController.getCurrentUser);
+router.patch(
+  "/avatars",
+  auth,
+  upload.single("avatar"),
+  userController.updateAvatar
+);
 
 module.exports = router;

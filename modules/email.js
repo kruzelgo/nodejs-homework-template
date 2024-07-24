e1fd2baafb562a682e749f99fec8d1ec1843d7ce
kruzelgo <kruzelgo@gmail.com>
2024-07-24 14:52:29 +0200

    Add Nodemailer integration for email sending

---
+++ modules/email.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+
+const { M_USER, M_PASS } = process.env;
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailgun.org",
+  port: 587,
+  secure: false,
+  auth: {
+    user: M_USER,
+    pass: M_PASS,
+  },
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+  const mailOptions = {
+    from: '"Mail Mail" <mailgun@com.com>',
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
+
+export default sendEmail;
